0f5d6edc9c33a1e9e3886a89d406bad17e8b8cf3
Marcos Corrochano Arroyo <mcorrochanoarroyo@gmail.com>
2024-07-03 01:31:31 +0200

    redis caching microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Player } from './players/entities/player.entity';
 import { ModerationModule } from './moderation/moderation.module';
 import { Ban } from './moderation/entities/ban.entity';
+import {
+  CacheModule,
+  CacheModuleAsyncOptions,
+  CacheStore,
+} from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
+import { CachingModule } from './caching/caching.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -16,6 +23,10 @@ import * as Joi from 'joi';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_DATABASE: Joi.string().required(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+        REDIS_USERNAME: Joi.string().required(),
+        REDIS_PASSWORD: Joi.string().required(),
       }),
     }),
     TypeOrmModule.forRootAsync({
@@ -24,7 +35,7 @@ import * as Joi from 'joi';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
+        port: +configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
@@ -32,8 +43,27 @@ import * as Joi from 'joi';
         synchronize: true,
       }),
     }),
+    CacheModule.registerAsync<CacheModuleAsyncOptions>({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const store = await redisStore({
+          socket: {
+            host: configService.get('REDIS_HOST'),
+            port: +configService.get('REDIS_PORT'),
+          },
+          username: configService.get('REDIS_USERNAME'),
+          password: configService.get('REDIS_PASSWORD'),
+        });
+        return {
+          store: store as unknown as CacheStore,
+        };
+      },
+      isGlobal: true,
+    }),
     PlayersModule,
     ModerationModule,
+    CachingModule,
   ],
   controllers: [],
   providers: [],
