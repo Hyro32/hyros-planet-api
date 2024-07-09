325432f05075adc48b52c80be3a651626c445517
Marcos Corrochano Arroyo <83825951+Hyro32@users.noreply.github.com>
2024-07-09 23:45:15 +0200

    @nestjs/config install

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+        REDIS_USERNAME: Joi.string().required(),
+        REDIS_PASSWORD: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
