1241bcf0005d2c2db2f6450e56687c255e7d5863
Marcos Corrochano Arroyo <83825951+Hyro32@users.noreply.github.com>
2024-08-07 13:42:32 +0200

    add astro-icon

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), icon()],
 });
